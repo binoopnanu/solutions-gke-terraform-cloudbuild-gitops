209737feba42224b4bf129ba5e2a06415737de1f
Bin Nan <binoop.nanu@pryzm.health>
2021-06-14 22:07:29 +1000

    adding tf configs

--- main.tf
+++ main.tf
@@ -33,6 +33,27 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
 }
 
+provider "helm" {
+  kubernetes {
+      host                   = "https://${google_container_cluster.primary.endpoint}"
+      token                  = data.google_client_config.default.access_token
+      cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+
+  }
+}
+
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress-controller"
+
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "nginx-ingress-controller"
+
+  set {
+    name  = "service.type"
+    value = "ClusterIP"
+  }
+}
+
 resource "google_service_account" "default" {
   account_id   = "service-account-id"
   display_name = "Service Account"
